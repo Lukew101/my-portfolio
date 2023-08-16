466b6d8894045f4d31fa22920c0919108b52a95e
Luke Williams <lukewilliams10101@gmail.com>
2023-08-16 15:13:30 +0200

    feat: services section complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -71,8 +71,27 @@ tabs.forEach(tab => {
     })
 })
 /*==================== SERVICES MODAL ====================*/
+const modalViews = document.querySelectorAll('.services__modal')
+const modalBtns = document.querySelectorAll('.services__button')
+const modalCloses = document.querySelectorAll('.services__modal-close')
 
+let modal = function(modalClick) {
+    modalViews[modalClick].classList.add('active-modal')
+}
 
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () => {
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
+        modalViews.forEach((modalView) => {
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
 /*==================== PORTFOLIO SWIPER  ====================*/
 
 
